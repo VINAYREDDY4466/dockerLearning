efe651d2c68af3dc1fd4303e344e3ececf8ad43b
VINAYREDDY4466 <dodlapativinayreddy4466@gmail.com>
2025-07-15 11:03:16 +0530

    first commit

---
+++ server2/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+
+const app =express();
+
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+    res.send("hello from the server two")
+});
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  if (username === 'admin' && password === '1234') {
+    return res.json({ message: `Login success from PORT ` });
+  }
+  res.status(401).json({ message: 'Invalid credentials' });
+});
+
+app.listen(3000, ()=>{
+    console.log("server is running on the port 3000")
+})
